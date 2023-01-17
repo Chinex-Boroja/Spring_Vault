663f4a2229f7a800491132e4771b24b3b7f83f9d
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2023-01-17 16:14:28 +0100

    Chore: simple mvc demo

---
+++ spring_starter/src/test/java/com/chinex/spring_starter/SpringStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chinex.spring_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
