280a354eb7c461af1435899a8c6bf6dd0f022c6e
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2023-01-25 13:32:44 +0100

    Added spring bean context

---
+++ spring_starter/src/main/java/com/chinex/spring_starter/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.chinex.spring_starter;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloWorld {
+
+    public void sayHello() {
+        System.out.println("Hello Boroja!!!");
+    }
+}
--- spring_starter/src/main/java/com/chinex/spring_starter/SpringStarterApplication.java
+++ spring_starter/src/main/java/com/chinex/spring_starter/SpringStarterApplication.java
@@ -2,6 +2,7 @@ package com.chinex.spring_starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,7 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class SpringStarterApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringStarterApplication.class, args);
+
+    ApplicationContext context = SpringApplication.run(SpringStarterApplication.class, args);
+    HelloWorld helloWorld = (HelloWorld) context.getBean("helloWorld");
+    helloWorld.sayHello();
+
     }
 
     // Add a method that sends a greeting
